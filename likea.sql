63da89dad549728124734081d00efd4cc8698e98
Hal <halschoolemail@gmail.com>
2024-12-05 17:18:36 -0600

    minor updates

--- likea.sql
+++ likea.sql
@@ -10,7 +10,6 @@ CREATE TABLE region(
 CREATE TABLE branch(
     branchID int AUTO_INCREMENT PRIMARY KEY,
     regionID int,
-    managerName varchar(50),
     branchPhoneNumber varchar(20),
     branchAddr varchar(60),
     FOREIGN KEY(regionID) REFERENCES region(regionID) ON DELETE CASCADE
